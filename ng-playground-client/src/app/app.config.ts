5e122ac7022356f6fa095212292b48217bbfe079
travis <travissomaroo2022@gmail.com>
2024-01-04 20:52:41 +0200

    Added prime ng

--- ng-playground-client/src/app/app.config.ts
+++ ng-playground-client/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(HttpClientModule)]
+  providers: [provideRouter(routes), importProvidersFrom([HttpClientModule, BrowserModule, BrowserAnimationsModule])]
 };
