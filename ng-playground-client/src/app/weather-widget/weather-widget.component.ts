15ef42bac1a4d2f1d04e1815c1e849c39bbb05b5
travis <travissomaroo2022@gmail.com>
2024-01-10 22:14:33 +0200

    Added more code

--- ng-playground-client/src/app/weather-widget/weather-widget.component.ts
+++ ng-playground-client/src/app/weather-widget/weather-widget.component.ts
@@ -1,19 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, TemplateRef } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { WidgetStateService } from './widget-state.service';
 import { WidgetActionsService } from './widget-actions.service';
 import { ButtonModule } from 'primeng/button';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-weather-widget',
   standalone: true,
   imports: [
     CardModule,
-    ButtonModule
+    ButtonModule,
+    NgTemplateOutlet
   ],
   templateUrl: './weather-widget.component.html',
 })
 export class WeatherWidgetComponent {
+  @Input()
+  headerTemplate: TemplateRef<any>;
+
+  @Input()
+  contentTemplate: TemplateRef<WidgetStateService>;
+
   state = inject(WidgetStateService);
   actions = inject(WidgetActionsService);
 }
