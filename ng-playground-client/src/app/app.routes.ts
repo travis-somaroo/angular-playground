b0b0cf3a2bfb5cf44afa0465c5de8b6545d7b23e
travis <travissomaroo2022@gmail.com>
2024-02-09 11:19:37 +0200

    Added rx state

--- ng-playground-client/src/app/app.routes.ts
+++ ng-playground-client/src/app/app.routes.ts
@@ -1,9 +1,28 @@
 import { Routes } from '@angular/router';
+import {
+  RecipeCarouselRxStateComponent
+} from './domain/rx-state-demo/recipe-carousel-rx-state/recipe-carousel-rx-state.component';
+import {
+  RecipeCarouselImperativeComponent
+} from './domain/imperative-demo/recipe-carousel/recipe-carousel-imperative.component';
 import { RecipeCarouselRxJSComponent } from './domain/rxjs-demo/recipe-carousel-rx-js/recipe-carousel-rx-js.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'rx-state',
+    component: RecipeCarouselRxStateComponent
+  },
+  {
+    path: 'imperative',
+    component: RecipeCarouselImperativeComponent
+  },
+  {
+    path: 'rxjs',
     component: RecipeCarouselRxJSComponent
+  },
+  {
+    path: '',
+    redirectTo: 'rx-state',
+    pathMatch: 'full'
   }
 ];
---
+++ ng-playground-client/src/app/domain/rx-state-demo/recipe-carousel-rx-state/recipe-carousel-rx-state.component.ts
@@ -0,0 +1,89 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { AsyncPipe, NgIf, NgTemplateOutlet } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { RecipePreviewComponent } from '../../../shared/recipe-preview/recipe-preview.component';
+import { RxState } from '@rx-angular/state';
+import { Recipe } from '../../../model/recipe';
+import { RecipeRepository } from '../../../data-access/recipe-repository.service';
+import { map, timer } from 'rxjs';
+import { select, selectSlice } from '@rx-angular/state/selections';
+
+interface State {
+  recipeIndex: number;
+  recipes: Recipe[];
+  index: number;
+}
+
+@Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-recipe-carousel-rx-state',
+  standalone: true,
+  imports: [
+    AsyncPipe,
+    ButtonModule,
+    NgIf,
+    NgTemplateOutlet,
+    RecipePreviewComponent
+  ],
+  providers: [RxState],
+  templateUrl: './recipe-carousel-rx-state.component.html',
+})
+export class RecipeCarouselRxStateComponent {
+  // We can pass the recipes into the component
+  @Input()
+  set recipes(recipes: Recipe[]) {
+    this.state.set({recipes});
+  }
+
+  recipeIndex$ = this.state.select('recipeIndex');
+  recipes$ = this.state.select('recipes');
+
+  recipe$ = this.state.select(
+    // Select slice to only update the recipes$ when the state in the select slice array changes
+    selectSlice(['recipeIndex', 'recipes']),
+    map(({recipes, recipeIndex}) => {
+      console.count('computing recipe...');
+      return recipes?.[recipeIndex];
+    }));
+
+  hasPrevious$ = this.recipeIndex$.pipe(
+    select(
+      map((i: number) => {
+        console.count('computing has previous...');
+        return i > 0;
+      })
+    )
+  );
+
+  // Select implements distinctUntilChanged and shareReplay with a ref count
+  hasNext$ = this.state.select(
+    selectSlice(['recipeIndex', 'recipes']),
+    map(({recipes, recipeIndex}) => {
+        console.count('computing has next...');
+        return recipeIndex + 1 < (recipes?.length ?? 0);
+      }
+    ));
+
+  constructor(private state: RxState<State>, private recipeRepository: RecipeRepository) {
+    this.state.set({recipeIndex: 0});
+    // Connect will sub to get recipes and put every emitted values in the state property, handles the sub for us
+    this.state.connect('recipes', this.recipeRepository.getRecipes$());
+    // Since hasNext$ and recipe$ is observing the whole state they will emit everytime the state changes.
+    this.state.connect('index', timer(0, 500));
+  }
+
+
+  onNext() {
+    // Either option is fine, first is more explicit
+    this.state.set('recipeIndex', ({recipeIndex}) => recipeIndex + 1);
+    // this.state.set(({recipeIndex}) => ({recipeIndex: recipeIndex + 1}))
+  }
+
+  onPrevious() {
+    this.state.set('recipeIndex', ({recipeIndex}) => recipeIndex - 1);
+  }
+
+  onReset() {
+    this.state.set({recipeIndex: 0});
+  }
+}
