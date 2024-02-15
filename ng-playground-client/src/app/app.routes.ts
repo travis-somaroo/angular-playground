ed04c7dac33ef4289f406de9be065407cab278d9
travis <travissomaroo2022@gmail.com>
2024-02-15 15:23:14 +0200

    Tweaks

--- ng-playground-client/src/app/app.routes.ts
+++ ng-playground-client/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { MyFormComponent } from './my-form/my-form.component';
+import { NgReactiveFormComponent } from './ng-reactive-form/ng-reactive-form/ng-reactive-form.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'signal-reactive-forms',
     component: MyFormComponent
+  },
+  {
+    path: 'ng-reactive-forms',
+    component: NgReactiveFormComponent
+  },
+  {
+    path: '',
+    redirectTo: 'ng-reactive-forms',
+    pathMatch: 'full'
   }
 ];
---
+++ ng-playground-client/src/app/ng-reactive-form/ng-reactive-form/ng-reactive-form.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { InputTextModule } from 'primeng/inputtext';
+import { JsonPipe, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-ng-reactive-form',
+  standalone: true,
+  imports: [
+    RadioButtonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    NgIf,
+    JsonPipe
+  ],
+  templateUrl: './ng-reactive-form.component.html',
+})
+export class NgReactiveFormComponent {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    gender: [],
+    otherGender: []
+  });
+
+
+  get isOtherGender() {
+    return this.form.controls.gender.value === 'other';
+  }
+}
