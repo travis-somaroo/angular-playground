9db76c3a883cf45533d88b5178a6e2d9e54083f1
travis <travissomaroo2022@gmail.com>
2024-03-12 13:47:41 +0200

    Tweaks

--- ng-playground-client/src/app/app.routes.ts
+++ ng-playground-client/src/app/app.routes.ts
@@ -2,12 +2,19 @@ import { Routes } from '@angular/router';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { adminGuard } from './auth/guards/admin.guard';
 import { ForbiddenComponent } from './error/components/forbidden/forbidden.component';
+import { ReportsComponent } from './admin/reports/reports.component';
 
 export const routes: Routes = [
   {
     path: 'admin',
     component: AdminDashboardComponent,
-    canActivate: [adminGuard]
+    canActivate: [adminGuard],
+    children: [
+      {
+        path: 'reports',
+        component: ReportsComponent
+      }
+    ]
   },
   {
     path: 'forbidden',
