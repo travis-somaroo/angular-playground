ea27fb1e284036217473d4b4ec3254d5399a76db
travis <travissomaroo2022@gmail.com>
2024-03-12 13:30:02 +0200

    Tweaks

--- ng-playground-client/src/app/auth/guards/admin.guard.ts
+++ ng-playground-client/src/app/auth/guards/admin.guard.ts
@@ -3,7 +3,5 @@ import { inject } from '@angular/core';
 import { UserService } from '../data-access/user.service';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  const service = inject(UserService);
-  const router = inject(Router);
-  return service.isAdmin() ? true : router.navigateByUrl('/forbidden');
+  return inject(UserService).isAdmin() ? true : inject(Router).navigateByUrl('/forbidden');
 };
