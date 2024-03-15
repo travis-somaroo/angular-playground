ac641727153b2e1723b68c37a2e6d8c839a8e94a
travis <travissomaroo2022@gmail.com>
2024-03-15 12:36:33 +0200

    Clean slate

--- ng-playground-client/src/app/dashboard/dashboard.component.ts
+++ ng-playground-client/src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { AnalyticsTileComponent } from '../components/analytics-tile/analytics-tile.component';
-import { LegendPosition } from '../components/analytics-tile/analytics-tile';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [
-    AnalyticsTileComponent
-  ],
+  imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
 
-  protected readonly LegendPosition = LegendPosition;
 }
