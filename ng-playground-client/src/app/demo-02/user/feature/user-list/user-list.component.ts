d2b85eab1ee06e3e3c0cc401577a4c4a7504a03f
travis <travissomaroo2022@gmail.com>
2024-03-26 14:27:45 +0200

    Done

--- ng-playground-client/src/app/demo-02/user/feature/user-list/user-list.component.ts
+++ ng-playground-client/src/app/demo-02/user/feature/user-list/user-list.component.ts
@@ -1,24 +1,28 @@
 import { Component, inject } from '@angular/core';
 import { UserListService } from '../../service/user-list.service';
-import { catchError, EMPTY } from 'rxjs';
+import { catchError, EMPTY, tap } from 'rxjs';
 import { DropdownModule } from 'primeng/dropdown';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
   imports: [
     DropdownModule,
-    AsyncPipe
+    AsyncPipe,
+    NgForOf,
+    NgIf
   ],
-  templateUrl: 'user-list.component.html'
+  templateUrl: 'user-list.component.html',
+  styleUrl: 'user-list.component.scss'
 })
 export class UserListComponent {
   private userService = inject(UserListService);
 
   errorMessage = '';
 
-  usersWithTodos$ = this.userService.usersWithTodos1$.pipe(
+  usersWithTodos$ = this.userService.usersWithTodos4$.pipe(
+    tap(console.log),
     catchError(err => {
       this.errorMessage = err;
       return EMPTY;
--- ng-playground-client/src/app/demo-02/user/service/user-list.service.ts
+++ ng-playground-client/src/app/demo-02/user/service/user-list.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ToDo, User, UserData } from '../model/user';
-import { catchError, forkJoin, map, mergeMap } from 'rxjs';
+import { catchError, delayWhen, forkJoin, map, mergeMap, of, retry, retryWhen, scan, tap, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,8 @@ export class UserListService {
   userUrl = User.userUrl;
   userUrlErr = User.userUrlErr;
 
+  // Try 1: Catch and rethrow, outer pipe
+  // If an error occurs, doesn't display any data
   usersWithTodos1$ = this.http.get<User[]>(this.userUrl).pipe(
     mergeMap(users => forkJoin(users.map(user =>
       this.http.get<ToDo[]>(`${this.userUrl}/${user.id}/todos`)
@@ -23,5 +25,103 @@ export class UserListService {
     )),
     catchError(User.handleError)
   );
+
+  // Try 2: Catch and rethrow, both pipes
+  // If an error occurs, doesn't display any data
+  usersWithTodos2$ = this.http.get<User[]>(this.userUrl).pipe(
+    mergeMap(users => forkJoin(users.map(user =>
+      this.http.get<ToDo[]>(`${this.userUrl}/${user.id}/todos`)
+        .pipe(
+          map(todos => ({
+            user,
+            todos
+          } as UserData)),
+          catchError(User.handleError)))
+    )),
+    catchError(User.handleError)
+  );
+
+  // Try 3: Catch and continue
+  // If an error occurs, displays data for user with no todos
+  // Will *not* re-catch in the UI to display a message
+  usersWithTodos3$ = this.http.get<User[]>(this.userUrl).pipe(
+    mergeMap(users => forkJoin(users.map(user =>
+      this.http.get<ToDo[]>(`${this.userUrl}/${user.id}/todos`)
+        .pipe(
+          map(todos => ({
+            user,
+            todos
+          } as UserData)),
+          catchError(err => of({
+            user,
+            todos: []
+          } as UserData))))
+    )));
+
+  // ? Try 4: Catch and continue and return message to UI (What we want)
+  usersWithTodos4$ = this.http.get<User[]>(this.userUrl).pipe(
+    mergeMap(users => forkJoin(users.map(user =>
+      this.http.get<ToDo[]>(`${this.userUrl}/${user.id}/todos`)
+        .pipe(
+          map(todos => ({
+            user,
+            todos
+          } as UserData)),
+          catchError(err => of({
+            user,
+            todos: [],
+            message: `Error retrieving todos for user: ${user.name}`
+          } as UserData))))
+    )));
+
+  // ? Try 5: Retry
+  usersWithTodos5$ = this.http.get<User[]>(this.userUrl).pipe(
+    mergeMap(users => forkJoin(users.map(user =>
+      this.http.get<ToDo[]>(`${this.userUrl}/${user.id}/todos`)
+        .pipe(
+          map(todos => ({
+            user,
+            todos
+          } as UserData)),
+          retry(3),
+          catchError(err => of({
+            user,
+            todos: [],
+            message: `Error retrieving todos for user: ${user.name}`
+          } as UserData))))
+    )));
+
+  // ? Try 6: RetryWhen
+  usersWithTodos6$ = this.http.get<User[]>(this.userUrl).pipe(
+    mergeMap(users => forkJoin(users.map(user =>
+      this.http.get<ToDo[]>(`${this.userUrl}/${user.id}/todos`)
+        .pipe(
+          map(todos => ({
+            user,
+            todos
+          } as UserData)),
+          retryWhen(error =>
+            error.pipe(
+              scan((acc, error) => {
+                if (acc > 3) {
+                  throw error;
+                }
+                return acc + 1;
+              }, 1),
+              // Extend the delay between retries
+              delayWhen(value => timer(value * 500)),
+              tap(value => console.log("Retry: ", value))
+            )
+          ),
+          catchError(err => {
+              console.log("Caught error");
+              return of({
+                user,
+                todos: [],
+                message: `Error retrieving todos for user: ${user.name}`
+              } as UserData);
+            }
+          )))
+    )));
 
 }
