74f90d2d8e3be692649217c714ca88469a6acfb8
unknown <travis@psybergate.co.za>
2025-09-09 15:51:43 +0200

    added cod

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,53 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
+import { NgClass, UpperCasePipe } from '@angular/common';
+
+interface Customer {
+  firstName: string;
+  lastName: string;
+  emailAddress: string;
+  status: 'active' | 'inactive';
+}
+
+function mapStatusToColor(status: 'active' | 'inactive'): string {
+  console.log('mapStatusToColor() running', status);
+  const colors: Record<string, string> = {
+    active: 'bg-green-100 text-green-500',
+    inactive: 'bg-red-100 text-red-500',
+  };
+  return colors[status];
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NgClass,
+    UpperCasePipe
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-playground');
+  protected readonly data = signal<Customer>({
+    firstName: 'John',
+    lastName: 'Doe',
+    emailAddress: 'john.doe@mail.com',
+    status: 'active',
+  });
+
+  // runs only when `this.data()` changes
+  protected readonly status = computed(() => mapStatusToColor(this.data().status));
+
+  // get called everytime change detection runs - done by angular
+  protected getStatus(status: 'active' | 'inactive'): string {
+    return mapStatusToColor(status);
+  }
+
+  protected markAsInactive(): void {
+    if (this.data().status === 'active') {
+      this.data.update(d => ({ ...d, status: 'inactive' }));
+    } else {
+      this.data.update(d => ({ ...d, status: 'active' }));
+    }
+  }
+
 }
